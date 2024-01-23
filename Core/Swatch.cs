f739d9c7b35f57d1f2d02629031eae211a85ebb9
Brandon Shumin <bjshumin@gmail.com>
2024-01-22 22:19:13 -0800

    Working split consoles

---
+++ Core/Swatch.cs
@@ -0,0 +1,52 @@
+﻿using RLNET;
+
+namespace RogueSharpV3Tutorial.Core
+{
+    internal class Swatch
+    {
+        // http://paletton.com/#uid=73d0u1kgLbtbGqNdPi8kZ7OpP44k6Cf53ImK58iR8capf39Ek5qgb2NnT41jT74c8bJ8Xkfg9b87fO8+9Yk28CoL7d
+
+        public static RLColor PrimaryLightest = new RLColor(86, 136, 127);
+        public static RLColor PrimaryLighter = new RLColor(52, 92, 85);
+        public static RLColor Primary = new RLColor(28, 58, 53);
+        public static RLColor PrimaryDarker = new RLColor(14, 39, 35);
+        public static RLColor PrimaryDarkest = new RLColor(4, 21, 18);
+
+        public static RLColor SecondaryLightest = new RLColor(116, 120, 126);
+        public static RLColor SecondaryLighter = new RLColor(93, 97, 105);
+        public static RLColor Secondary = new RLColor(72, 77, 85);
+        public static RLColor SecondaryDarker = new RLColor(51, 56, 64);
+        public static RLColor SecondaryDarkest = new RLColor(31, 38, 47);
+
+        public static RLColor AlternateLightest = new RLColor(126, 114, 94);
+        public static RLColor AlternateLighter = new RLColor(79, 71, 57);
+        public static RLColor Alternate = new RLColor(73, 60, 38);
+        public static RLColor AlternateDarker = new RLColor(69, 53, 26);
+        public static RLColor AlternateDarkest = new RLColor(57, 41, 13);
+
+        public static RLColor ComplimentLightest = new RLColor(181, 169, 160);
+        public static RLColor ComplimentLighter = new RLColor(150, 136, 126);
+        public static RLColor Compliment = new RLColor(120, 105, 95);
+        public static RLColor ComplimentDarker = new RLColor(83, 70, 62);
+        public static RLColor ComplimentDarkest = new RLColor(77, 55, 41);
+
+        // http://pixeljoint.com/forum/forum_posts.asp?TID=12795
+
+        public static RLColor DbDark = new RLColor(20, 12, 28);
+        public static RLColor DbOldBlood = new RLColor(68, 36, 52);
+        public static RLColor DbDeepWater = new RLColor(48, 52, 109);
+        public static RLColor DbOldStone = new RLColor(78, 74, 78);
+        public static RLColor DbWood = new RLColor(133, 76, 48);
+        public static RLColor DbVegetation = new RLColor(52, 101, 36);
+        public static RLColor DbBlood = new RLColor(208, 70, 72);
+        public static RLColor DbStone = new RLColor(117, 113, 97);
+        public static RLColor DbWater = new RLColor(89, 125, 206);
+        public static RLColor DbBrightWood = new RLColor(210, 125, 44);
+        public static RLColor DbMetal = new RLColor(133, 149, 161);
+        public static RLColor DbGrass = new RLColor(109, 170, 44);
+        public static RLColor DbSkin = new RLColor(210, 170, 153);
+        public static RLColor DbSky = new RLColor(109, 194, 202);
+        public static RLColor DbSun = new RLColor(218, 212, 94);
+        public static RLColor DbLight = new RLColor(222, 238, 214);
+    }
+}
